93b0b49ae6091823195f2acf55fa07ea81254a24
Wendy Reyes <wendy.reyessan@gmail.com>
2017-05-29 17:42:39 -0500

    subiendo footer

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,33 @@
 
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 18
+  });
+  var infoWindow = new google.maps.InfoWindow({map: map});
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent("Ud está aquí");
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Si el navegador no es compatible:
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: El servicio Geolocation falló.' :
+                        'Error: Tu navegador no soporta Geolocation.');
+}
