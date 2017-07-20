4df6180424322584721624d09c3d559bef1b6192
flordeciruela <wendy.reyessan@gmail.com>
2017-07-20 04:22:38 -0500

    edit nav

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,10 @@
-
+var map, infoWindow, ubicacion;
 function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -12.1188, lng: -77.037},
     zoom: 18
   });
-  var infoWindow = new google.maps.InfoWindow({map: map});
+  infoWindow = new google.maps.InfoWindow;
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
@@ -14,20 +14,61 @@ function initMap() {
       };
 
       infoWindow.setPosition(pos);
-      infoWindow.setContent("Ud está aquí");
+      infoWindow.setContent('Location found.');
       map.setCenter(pos);
+      console.log(pos);
+      var marker = new google.maps.Marker({
+        position: pos,
+        map: map
+      });
+
     }, function() {
       handleLocationError(true, infoWindow, map.getCenter());
     });
   } else {
-    // Si el navegador no es compatible:
     handleLocationError(false, infoWindow, map.getCenter());
   }
+
+  //autocompletePartida
+  var inputSalida = document.getElementById('input-partida');
+  var autocompletePartida = new google.maps.places.Autocomplete(inputSalida);
+  autocompletePartida.bindTo('bounds', map);
+
+  var infowindow = new google.maps.InfoWindow();
+  var infowindowContent = document.getElementById('infowindow-content');
+  infowindow.setContent(infowindowContent);
+  autocompletePartida.addListener('place_changed', function() {
+    infowindow.close();
+    var place = autocompletePartida.getPlace();
+    if (!place.geometry) {
+      window.alert("No details available for input: '" + place.name + "'");
+      return;
+    }
+  });
+
+  //autocompleteLlegada
+  var inputLlegada = document.getElementById('input-llegada');
+  var autocompleteLlegada = new google.maps.places.Autocomplete(inputLlegada);
+  autocompleteLlegada.bindTo('bounds', map);
+
+  var infowindow = new google.maps.InfoWindow();
+  var infowindowContent = document.getElementById('infowindow-content');
+  infowindow.setContent(infowindowContent);
+  autocompleteLlegada.addListener('place_changed', function() {
+    infowindow.close();
+    var place = autocompleteLlegada.getPlace();
+    if (!place.geometry) {
+      window.alert("No details available for input: '" + place.name + "'");
+      return;
+    }
+  });
+
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   infoWindow.setPosition(pos);
   infoWindow.setContent(browserHasGeolocation ?
-                        'Error: El servicio Geolocation falló.' :
-                        'Error: Tu navegador no soporta Geolocation.');
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(map);
 }
